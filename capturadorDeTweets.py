875ff8fa37e5e38d096ddc3580bbc5a903a85e34
Laura Sofia Muñoz Montoya <muozmontoyal@gmail.com>
2017-11-21 20:42:48 -0500

    Creando diferentes datasets

--- capturadorDeTweets.py
+++ capturadorDeTweets.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import sys
+import sys,re,json
 import tweepy,csv,os
 
 #Claves de autenticacion
@@ -18,7 +18,6 @@ api = tweepy.API(auth)
 print('El siguiente script permite ingresar un termino de busqueda y buscarlo en Twitter, pueden ser hashtags\n')
 stdin = (raw_input('Ingrese el termino de busqueda :\n'))
 stdin.encode('UTF-8')
-
 #Esto quita el archivo si ya existe
 #try:
     #os.remove("mycsvfile.csv")
@@ -26,26 +25,25 @@ stdin.encode('UTF-8')
     #pass
 
 try:
-	#Crea el set de tweets dependiendo de lo que se busco
-	sQuery = tweepy.Cursor(api.search, q=stdin, geocode="6.2690007,-75.7364814,100km",show_user=True).items(10)
-	writedHeader = True
-	for tweet in sQuery:
-
-		#Creo el tweet como un diccionario
-		tweet_data = {}
-		tweet_data["texto"] = tweet._json["text"].encode("UTF-8")
-		tweet_data["id"] = tweet._json["id"]
-		tweet_data["lang"] = tweet._json["lang"].encode("UTF-8")
-
+    #Crea el set de tweets dependiendo de lo que se busco
+    sQuery = tweepy.Cursor(api.search, q=stdin, geocode="6.2690007,-75.7364814,100km",show_user=True).items(10)
+    writedHeader = True
+    for tweet in sQuery:
+        stdin=stdin.replace(" ", "-")
+        #Creo el tweet como un diccionario
+        tweet_data = {}
+        tweet_data["texto"] = tweet._json["text"].encode("UTF-8")
+        tweet_data["id"] = tweet._json["id"]
+        tweet_data["lang"] = tweet._json["lang"].encode("UTF-8")
+        print json.dumps(tweet._json)
 		#Abro el csv y le hago append de ese tweet, solo escribo el header la primera vez mediante la variable de control
-		with open('mycsvfile.csv', 'a') as f:
+        with open(stdin+'_dataset.csv', 'a') as f:
 			#El header seran las keys de ese tweet (que son las mismas para todos los tweets)
-			w = csv.DictWriter(f, tweet_data.keys())
-			if writedHeader:
-				w.writeheader()
-				writedHeader = False
-			w.writerow(tweet_data)
+            w = csv.DictWriter(f, tweet_data.keys())
+            if writedHeader:
+                w.writeheader()
+                writedHeader = False
+            w.writerow(tweet_data)
 except tweepy.TweepError:
-	print('You have an error on your auth, please check your keys')
-
-#Metodo clacificar
+    print('You have an error on your auth, please check your keys')
+#Metodo clasificar

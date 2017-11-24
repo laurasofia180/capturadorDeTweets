8cf01d6e4804c5bc40792a78949dc7e8238d522f
Laura Sofia Muñoz Montoya <muozmontoyal@gmail.com>
2017-11-24 00:29:06 -0500

    Añadiendo hashtags al csv

--- capturadorDeTweets.py
+++ capturadorDeTweets.py
@@ -10,7 +10,7 @@ consumer_secret = "1NBIAkp3hG0lbQGGraig2AIuxahDwqOTiLA3P2OTAYqggzMjaP"
 access_token = "70793170-DZRt56LsQDd5y01xuZL3JSl8WiceEwk981ufrqdXw"
 access_token_secret = "ptES1q1alDlxTPD91pwPXAfNkWcOljmVt5rfDbFOYoEq6"
 
-# Appi de tweetpy para coger datos de tweeter
+# API de tweetpy para coger datos de tweeter
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
@@ -20,15 +20,9 @@ print('El siguiente script permite ingresar un termino de busqueda y buscarlo en
 stdin = (raw_input('Ingrese el termino de busqueda :\n'))
 stdin.encode('UTF-8')
 
-# Esto quita el archivo si ya existe
-# try:
-# os.remove("mycsvfile.csv")
-# except OSError:
-# pass
-
 try:
     # Crea el set de tweets dependiendo de lo que se busco
-    sQuery = tweepy.Cursor(api.search, q=stdin, geocode="6.2690007,-75.7364814,100km", show_user=True).items(10)
+    sQuery = tweepy.Cursor(api.search, q=stdin, geocode="6.2690007,-75.7364814,100km", show_user=True).items(50)
     writedHeader = True
 
     # Comprobacion de si ya existe ese archivo
@@ -41,18 +35,26 @@ try:
     else:
         print ("el archivo no existe")
 
+    #Itero los resultados de la busqueda y creo los datos correspondientes
     for tweet in sQuery:
         stdin = stdin.replace(" ", "-")  # Remplaza un espacio por un guion de ser necesario
         if (datetime.datetime.now() - tweet.created_at).days < 365:
             # Creo el tweet como un diccionario
             tweet_data = {}
-            tweet_data["texto"] = tweet._json["text"].encode("UTF-8")
             tweet_data["id"] = tweet._json["id"]
             tweet_data["lang"] = tweet._json["lang"].encode("UTF-8")
-            tweet_data["Fecha"] = tweet._json["created_at"].encode("UTF-8")
-            tweet_data["Ubicacion"] = tweet.user.location.encode("UTF-8")
-            # print json.dumps(tweet._json)
-
+            tweet_data["texto"] = tweet._json["text"].encode("UTF-8")
+            tweet_data["fecha"] = tweet._json["created_at"].encode("UTF-8")
+            # Si el tweet tiene hashtags los convierto en una lista de hashtags, sino vacio
+            if len(tweet.entities["hashtags"]):
+                hashtagList = []
+                for hashtag in tweet.entities["hashtags"]:
+                    hashtagList.append(hashtag["text"])
+                hashtagList = ','.join(map(str, hashtagList))
+                tweet_data["hashtags"] = hashtagList
+            else:
+                tweet_data["hashtags"] = ""
+            tweet_data["ubicacion"] = tweet.user.location.encode("UTF-8")
             # Abro el csv y le hago append de ese tweet, solo escribo el header la primera vez mediante la variable de control
             with open(datasetName, 'a') as f:
                 # El header seran las keys de ese tweet (que son las mismas para todos los tweets)
